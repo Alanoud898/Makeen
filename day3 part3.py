f774f78c96659c0ee8a95017d63686b56a84f76d
Alanoud898 <132902270+Alanoud898@users.noreply.github.com>
2023-05-10 15:17:22 +0400

    Delete day3 part3.py

--- day3 part3.py
+++
@@ -1,8 +0,0 @@
-world1=(input("Enter world1:"))
-world_length=len(world1)
-if world_length %2==0:
-    print("middle charactrrs")
-    print(world1 + "middle charactrrs:")
-else:
-    print(world1)
-
