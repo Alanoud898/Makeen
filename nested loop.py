f4a777088fc1a3108a72fa0eb8d46a4048f59164
Alanoud898 <132902270+Alanoud898@users.noreply.github.com>
2023-05-11 13:52:22 +0400

    Add files via upload

---
+++ nested loop.py
@@ -0,0 +1,16 @@
+for i in range(3):
+    for j in range(4):
+        print("[]",end=" ")
+    print()
+    
+    
+m="|rx#duh#juhdwh"
+
+for i in m:
+    n=ord(i)-3
+    n2=chr(n)
+    
+    
+       
+    print(n2,end='')
+        
