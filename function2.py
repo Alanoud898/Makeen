a45f66723a52e0ac1bab76b45c635d2579918078
Alanoud898 <132902270+Alanoud898@users.noreply.github.com>
2023-05-15 09:47:08 +0400

    Add files via upload

---
+++ function2.py
@@ -0,0 +1,10 @@
+"""def str_digit(n):
+    sum=0
+    for i in str(n):
+        str+=int(i)
+        return sum
+    n=input("enter the num")
+    print(str_digit(n))"""
+x,y,z=(input("Enter the num:").split())
+sum=(int(x)+int(y)+int(z))
+print(sum)
