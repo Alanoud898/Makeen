586769526832da8173ec96f336050451383125fa
Alanoud898 <132902270+Alanoud898@users.noreply.github.com>
2023-05-16 03:54:31 -0700

    Add files via upload

---
+++ day3.2iiii.py
@@ -0,0 +1,16 @@
+def main():
+    sideInput=int(input("Enter the side num:"))
+    counters=countInput(sideInput)
+    print(counters)
+main()
+def count_input(side):
+    counters=[0]=(s+1)
+    for item in range(slideInput):
+        roll_result=int(input("Enter the num of roll:"))
+     if  roll_result<=6
+              counter[roll_result]= counters[roll_result]+1
+    return counters
+   
+
+
+    
