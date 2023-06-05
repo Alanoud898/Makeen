c7aa257bf711bf3db3fae6c26719990726b0943e
Alanoud898 <132902270+Alanoud898@users.noreply.github.com>
2023-06-05 12:17:46 -0700

    Add files via upload

---
+++ one1.py
@@ -0,0 +1,55 @@
+#extend  and append in py
+"""name= ["A","B","C","D"]
+name2=["a","b","c"]
+#name.extend(name2)
+#print(name)
+name.append(name2)
+print(name)"""
+
+"""name= ["A","B","C","D"]
+name2=["a","b","c"]
+name.extend(name2)
+name.append("E")
+print(name)"""
+
+#insert fun
+"""name= ["A","B","C","D"]
+name2=["a","b","c"]
+name.extend(name2)
+name.insert(5,"E")
+print(name)"""
+
+#remove fun
+"""name= ["A","B","C","D"]
+name2=["a","b","c"]
+name.extend(name2)
+name.remove("B")
+print(name)"""
+
+#pop fun يحذف اخر عنصر في القائمة
+"""name= ["A","B","C","D"]
+name2=["a","b","c"]
+name.extend(name2)
+name.pop()
+print(name)"""
+
+#index
+"""name= ["A","B","C","D"]
+name2=["a","b","c"]
+name.extend(name2)
+name.index("b")
+print(name.index("b"))
+print(name)"""
+
+#count
+"""name= ["A","B","C","D"]
+name2=["a","b","c","A"]
+name.extend(name2)
+name.count("A")
+print(name.count("A"))
+print(name)"""
+
+
+
+
+
