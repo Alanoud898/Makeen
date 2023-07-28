0e805c8b2fe2805c9d1aed1d53217775c05628ce
Alanoud898 <132902270+Alanoud898@users.noreply.github.com>
2023-07-28 02:14:12 -0700

    Add files via upload

---
+++ Score.R
@@ -0,0 +1,13 @@
+score <- 89
+
+if (score >= 90 && score <= 100) {
+  print("A")
+} else if (score >= 80 && score <= 89) {
+  print("B")
+} else if (score >= 70 && score <= 79) {
+  print("C")
+} else if (score >= 60 && score <= 69) {
+  print("D")
+} else {
+  print("Invalid score")
+}
